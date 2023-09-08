68290ddf11974fa087ebd63e1a3fe01b371716b8
Abdelrahman74S <bdalrhmnr927@gmail.com>
2023-09-08 13:14:42 -0700

    if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,14 +5,17 @@
  *
  * Return: Always 0.
 */
-int main() {
+int main(void)
+{
 char letter = 'a';
-while (letter <= 'z') {
-if (letter != 'q' && letter != 'e') {
+while (letter <= 'z')
+{
+if (letter != 'q' && letter != 'e')
+{
 putchar(letter);
 }
 letter++;
 }
 putchar('\n');
-return 0;
+return (0);
 }
