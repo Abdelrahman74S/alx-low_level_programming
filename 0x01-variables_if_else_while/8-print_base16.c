8ce537c9b60b4968dc01b93b2b0aa885fa640824
Abdelrahman74S <bdalrhmnr927@gmail.com>
2023-09-08 14:20:14 -0700

    if

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,18 +6,16 @@
  *return: Always 0:
  *
 */
+
 int main(void)
 {
-int n;
-char digit;
-for (n = 0; n < 10; n++)
-{
-putchar((n % 10) + '0');
-}
-for (digit = 'a'; digit <= 'f'; digit++)
-{
-putchar(digit);
-}
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
+
